53f36101a219b2325fc0568a1df2b46ec266d685
Addison <addison@framer.com>
2019-11-21 16:31:36 +0100

    Update JSONViewer

--- code/Farfetch.tsx
+++ code/Farfetch.tsx
@@ -1,16 +1,51 @@
 import * as React from "react";
-import { Frame, addPropertyControls, ControlType } from "framer";
+import { Frame, FrameProps, addPropertyControls, ControlType } from "framer";
 //@ts-ignore
 import { Button } from "./Props.tsx";
+import Clipboard from "react-clipboard.js";
 
-export function MyButton(props) {
+import { useGlobal } from "./JSONViewer";
+
+type Props = Partial<FrameProps> &
+  Partial<{
+    stateValue: string;
+    displayOptions: string;
+    buttonType: string;
+    stretch: string;
+  }>;
+
+export const MyButton = (props: Props) => {
+  const { ...rest } = props;
+  const [globalState, globalActions] = useGlobal();
+
+  let ButtonData = {
+    id: props.id,
+    type: "button_component",
+    displayOptions: [
+      { key: "button-type", value: props.buttonType },
+      { key: "stretch", value: props.stretch },
+      { key: "horizontal-alignment", value: "center" }
+    ],
+    data: [{ key: "text", value: "dead" }]
+  };
+
+  React.useLayoutEffect(() => {
+    globalActions.addItem(ButtonData);
+  }, [props]);
+
   return (
-    <Frame {...Button(props)}>
+    <Frame {...rest}>
       <h1>{props.buttonType}</h1>
       <h1>{props.stretch}</h1>
     </Frame>
   );
-}
+};
+
+MyButton.defaultProps = {
+  displayOptions: "false",
+  buttonType: "Primary",
+  stretch: "fill"
+};
 
 addPropertyControls(MyButton, {
   buttonType: {
